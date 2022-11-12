bdf3417b6f249a1586d5f4578dab22bc345ecd51
Maksim Frankov <2m.franko@gmail.com>
2022-11-12 11:52:50 +0200

    edits

--- js/task-05.js
+++ js/task-05.js
@@ -2,8 +2,10 @@ const textInput = document.querySelector("#name-input");
 const textOutput = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {
-     if (event.currentTarget.value === "") {
+    if (event.currentTarget.value == "") {
         textOutput.textContent = 'Anonymous'
     }
-    textOutput.textContent = event.currentTarget.value;
-});
+    else if (event.currentTarget.value != "")  {
+        textOutput.textContent = event.currentTarget.value;
+    }
+    });
--- js/task-08.js
+++ js/task-08.js
@@ -1,13 +1,22 @@
 const formLogin = document.querySelector('.login-form');
-
+let   data = {
+        Email: null,
+        Password: null,
+}
 formLogin.addEventListener('submit', (event) => {
     event.preventDefault();
     const {
         elements: { email, password }
     } = event.currentTarget;
     if (email.value === "" || password.value === "") {
-        return console.log("Поле не заполнено!");
-    } else
-        console.log(`Email: ${email.value}, Password: ${password.value}`);
+        return alert("Поле не заполнено!");
+    }
+    data = {
+        Email: email.value,
+        Password: password.value,
+    }
+    alert(JSON.stringify(data, null, 3));
     event.currentTarget.reset();
+
+    
 });
